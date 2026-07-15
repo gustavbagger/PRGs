b74a775966f3833eb866c221ed15731efe7cbaa8
gustavbagger <gustavbagger@me.com>
2026-07-15 16:06:47 +1000

    sympy implimentation

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-from math import *
-from sympy import mod_inverse
+import math as ma
+import numpy as np
+from sympy import mod_inverse, Matrix, zeros
 
+def helperArrayAdderSP(row: int, columns: list, matrix: Matrix) -> None:
+    matrix[row,columns[0]-1]+=1
+    matrix[row,columns[1]-1]+=1
+    matrix[row,columns[2]-1]+=1
+    matrix[row,columns[3]-1]+=1
+    return
+
+def AdjecencyMatrixSP(mod: int, prim: int) -> Matrix:
+    out = zeros(mod-1)
+    primInv = mod_inverse(prim,mod)
+    helperArrayAdderSP(0,[mod-1,2,primInv,prim],out)
+    for i in range(2,mod-1):
+        helperArrayAdderSP(i-1,[i-1,i+1,(i*primInv) % mod,(i*prim) % mod],out)
+    helperArrayAdderSP(mod-2,[mod-2,1,mod-primInv,mod-prim],out)
+    return out
+
+
+def main():
+    print(AdjecencyMatrixSP(13,2))
+    return
+main()
+
+
+#depricated:
+"""
 def AdjecencyMatrixNaive(mod: int, prim: int) -> list:
     out = list()
     primInv = mod_inverse(prim,mod)
@@ -13,7 +39,19 @@ def AdjecencyMatrixNaive(mod: int, prim: int) -> list:
     out.append(Adjecency)
     return out
 
-def main():
-    print(AdjecencyMatrixNaive(13,2))
+def helperArrayAdderNP(row: int, columns: list, array: np.ndarray) -> None:
+    array[row,columns[0]-1]+=1
+    array[row,columns[1]-1]+=1
+    array[row,columns[2]-1]+=1
+    array[row,columns[3]-1]+=1
     return
-main()
+
+def AdjecencyMatrixNP(mod: int, prim: int) -> np.ndarray:
+    out = np.zeros((mod-1,mod-1))
+    primInv = mod_inverse(prim,mod)
+    helperArrayAdderNP(0,[mod-1,2,primInv,prim],out)
+    for i in range(2,mod-1):
+        helperArrayAdderNP(i-1,[i-1,i+1,(i*primInv) % mod,(i*prim) % mod],out)
+    helperArrayAdderNP(mod-2,[mod-2,1,mod-primInv,mod-prim],out)
+    return out
+"""
