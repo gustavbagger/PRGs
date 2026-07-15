abb6939ff9b976d72449b2d8ecf5a60ed2514e81
gustavbagger <gustavbagger@me.com>
2026-07-15 15:12:37 +1000

    Adjecency matrix

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from math import *
+from sympy import mod_inverse
 
-def funct():
+def AdjecencyMatrixNaive(mod: int, prim: int) -> list:
+    out = list()
+    primInv = mod_inverse(prim,mod)
+    Adjecency = sorted([mod-1,2,primInv,prim])
+    out.append(Adjecency)
+    for i in range(2,mod-1):
+        Adjecency = sorted([i-1,i+1,(i*primInv) % mod,(i*prim) % mod])
+        out.append(Adjecency)
+    Adjecency = sorted([mod-2,1,mod-primInv,mod-prim])
+    out.append(Adjecency)
+    return out
+
+def main():
+    print(AdjecencyMatrixNaive(13,2))
     return
+main()
